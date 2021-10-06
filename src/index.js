0d14222c94db11989f69afc6f7ef749fddb9962c
Saara Pekkala <saara.pekkala@eficode.com>
2021-10-06 14:00:25 +0300

    remove comments

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import './index.css';
 import theme from './theme';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
-//import swUrl from './service-worker.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,11 +16,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 // Register service worker
 
