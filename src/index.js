175f55e418f1216890cfc2db6cfaa20dd0b91917
Saara Pekkala <saara.pekkala@eficode.com>
2021-10-06 13:56:03 +0300

    remove unused files, configure service worker with caching

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import './index.css';
 import theme from './theme';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+//import swUrl from './service-worker.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -25,4 +22,18 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// Register service worker
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register(`sw.js`);
+      console.log('Service worker registered', registration);
+    } catch (err) {
+      console.log('Service worker registration failed', err);
+    }
+  });
+}
+
